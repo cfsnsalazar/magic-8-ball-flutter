9acfbd08d4f9a619e3f4c40ac349b97021c988b7
Napoleon Salazar <nsalazar@coolfiresolutions.com>
2020-05-27 16:58:34 -0500

    Boss level 1 challenge completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+          backgroundColor: Colors.blue[900], title: Text('Ask Me anything')),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(4);
+          });
+          print('I got clicked');
+        },
+        child: Image.asset('images/ball${ballNumber + 1}.png'),
+      ),
+    );
+  }
+}
